c7092577788828df351016b387001d184e93a5f7
SahilMadan <Hillatio@gmail.com>
2020-03-05 16:13:00 -0800

    Add Cpu class skeleton code to project.

---
+++ nes_emu/cpu.cc
@@ -0,0 +1,9 @@
+#include "cpu.h"
+
+namespace nes_emu {
+namespace system {
+
+Cpu::Cpu() {}
+
+}  // namespace system
+}  // namespace nes_emu
---
+++ nes_emu/cpu.h
@@ -0,0 +1,15 @@
+#ifndef NES_EMU_SYSTEM_CPU_H_
+#define NES_EMU_SYSTEM_CPU_H_
+
+namespace nes_emu {
+namespace system {
+
+class Cpu {
+ public:
+  Cpu();
+};
+
+}  // namespace system
+}  // namespace nes_emu
+
+#endif
